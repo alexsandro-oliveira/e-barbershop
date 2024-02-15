adee87c0d6d40c33658b98b8ce97d2afe3b1e6fc
Alexs <ale.osantos@hotmail.com>
2024-02-14 21:31:35 -0300

    feat: ensure user can search for barbershops

--- app/(home)/_components/search.tsx
+++ app/(home)/_components/search.tsx
@@ -1,16 +1,67 @@
 "use client";
 
 import { Button } from "@/app/_components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/app/_components/ui/form";
 import { Input } from "@/app/_components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { SearchIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+
+import { z } from "zod";
+
+const formSchema = z.object({
+  search: z
+    .string({
+      required_error: "Campo obrigatório.",
+    })
+    .trim()
+    .min(1, "Campo obrigatório."),
+});
 
 const Search = () => {
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  const handleSubmit = (data: z.infer<typeof formSchema>) => {
+    router.push(`/barbershops?search=${data.search}`);
+  };
+
   return (
     <div className="flex items-center gap-2">
-      <Input placeholder="Busque por uma barbearia" />
-      <Button variant="default" size="sm">
-        <SearchIcon size={20} />
-      </Button>
+      <Form {...form}>
+        <form
+          className="flex w-full gap-4"
+          onSubmit={form.handleSubmit(handleSubmit)}
+        >
+          <FormField
+            control={form.control}
+            name="search"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Input placeholder="Busque por uma barbearia..." {...field} />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button variant="default" type="submit">
+            <SearchIcon size={20} />
+          </Button>
+        </form>
+      </Form>
     </div>
   );
 };
